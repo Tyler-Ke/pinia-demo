6c290d18e0eeb9baea1876f6769de68618ace801
Tyler Ke <tdkeytalt@gmail.com>
2025-08-07 10:54:55 -0500

    added new store of gameStore.js which stores data for game

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
+app.use(router);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.mount("#app");
