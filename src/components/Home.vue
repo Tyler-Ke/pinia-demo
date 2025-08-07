b1032c8c540781d290127bec6b6b48ffae7b47e7
Tyler Ke <tdkeytalt@gmail.com>
2025-08-07 11:00:14 -0500

    added some buttons for the game to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,21 @@
 <template>
-   Home
+   <div class="text-center">
+      <p>Let's play the game</p>
+      <h2 class="text-primary pb-3">Current Score</h2>
+      <span class="text-primary" pb-3>Max Score:</span>
+      <br />
+      <div class="row">
+         <div class="col-5 offset-1">
+            <button class="form-control btn btn-success p-4">Increment</button>
+         </div>
+         <div class="col-5 offset-1">
+            <button class="form-control btn btn-danger p-4">Decrement</button>
+         </div>
+         <div class="col-6 offset-3 pt-2">
+            <button class="form-control btn btn-warning p-4">Random</button>
+         </div>
+      </div>
+   </div>
 </template>
+
+<script setup></script>
